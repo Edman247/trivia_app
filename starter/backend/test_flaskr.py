27296487cbd03cf38a70a8884aa2f21e07a67bd4
Edman247 <redmanuel247@gmail.com>
2021-03-22 16:00:43 -0400

    test_get_questions

--- starter/backend/test_flaskr.py
+++ starter/backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}:{}@{}/{}".format('postgres', 'Rem051992','localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,11 +24,24 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
+        self.new_question = {
+            'question': 'Who is the current president of US?',
+            'answer': 'Joe Biden',
+            'category': '1',
+            'difficulty': 1
+        }
+
     def tearDown(self):
         """Executed after reach test"""
         pass
 
+    def test_get_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+        print(data)
+
+        self.assertEqual(res.status_code, 200)
     """
     TODO
     Write at least one test for each test for successful operation and for expected errors.
